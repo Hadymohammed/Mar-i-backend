324b6321f4c3384d57e728ff7f63b00c6e840c70
Abdelhady <hadymohamed2002@gmail.com>
2025-12-14 14:44:31 +0200

    add: setter & getter

--- src/common/services/redis.service.ts
+++ src/common/services/redis.service.ts
@@ -28,4 +28,22 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   getClient() {
     return this.client;
   }
+
+  async set(key: string, value: string, expiresInSeconds?: number): Promise<void> {
+    if (expiresInSeconds) {
+      await this.client.SET(key, value, {
+        EX: expiresInSeconds,
+      });
+    } else {
+      await this.client.SET(key, value);
+    }
+  }
+
+  async get(key: string): Promise<string | null> {
+    return (await this.client.GET(key)) as string | null;
+  }
+
+  async del(key: string): Promise<number> {
+    return (await this.client.DEL(key)) as number;
+  }
 }
